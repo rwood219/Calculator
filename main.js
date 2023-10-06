90607534f3164b4c63506a848cca66753199e267
rwood219 <58787084+rwood219@users.noreply.github.com>
2023-10-05 21:52:43 -0400

    changes to css and html

--- main.js
+++ main.js
@@ -1,38 +1,36 @@
-let buttons = document.querySelectorAll('button');
-let display = document.querySelector('.previous-number');
-let solution = document.querySelector('.current-number')
+let buttons = document.querySelectorAll("button");
+let display = document.querySelector(".previous-number");
+let solution = document.querySelector(".current-number");
 
-for(i=0; i< buttons.length; i++) {
-    buttons[i].addEventListener('click', (e)=> {
-        currentNum = e.target.innerHTML
-        if(currentNum === 'C'){
-           clear()
-        } else if(currentNum === "=") {         
-           solve()
-        }else if(currentNum === 'DEL'){
-            del(currentNum)
-        }
-        else {
-            display.innerHTML +=  currentNum 
-        }
-    })
-};
-
-solve = (x, y) => {
-  solution.innerHTML = (eval(display.innerHTML)) 
+for (i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", (e) => {
+    currentNum = e.target.innerHTML;
+    if (currentNum === "C") {
+      clear();
+    } else if (currentNum === "=") {
+      solve();
+    } else if (currentNum === "DEL") {
+      del(currentNum);
+    } else {
+      display.innerHTML += currentNum;
+    }
+  });
 }
 
-//solve on enter press 
-window.addEventListener('keydown', (e) => {
-    console.log('enter press')
-})
+solve = (x, y) => {
+  solution.innerHTML = eval(display.innerHTML);
+};
+
+//solve on enter press
+window.addEventListener("keydown", (e) => {
+  console.log("enter press");
+});
 // clear display
 clear = () => {
-    display.innerHTML = " " 
-    solution.innerHTML = " "
+  display.innerHTML = " ";
+  solution.innerHTML = " ";
 };
 //delete last number entered
 del = () => {
-    display.innerHTML = display.innerHTML.replace(/.$/, "")
+  display.innerHTML = display.innerHTML.replace(/.$/, "");
 };
-
